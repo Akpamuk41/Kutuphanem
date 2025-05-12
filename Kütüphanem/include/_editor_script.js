7289e5137d87bef446ddf5a69cfc9e19bba15068
Orazjemal Meredova <pamukmeredova@gmail.com>
2025-05-12 16:06:15 +0300

    yeni

--- Kütüphanem/include/_editor_script.js
+++
@@ -1,9 +0,0 @@
-<!-- Include the Quill library -->
-<script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
-
-<!-- Initialize Quill editor -->
-<script>
-  const quill = new Quill('#editor', {
-    theme: 'snow'
-  });
-</script>
